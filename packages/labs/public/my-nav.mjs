eef785a66281682a1ebdbeade57258689003655b
Peter Ryan <147430628+peterryan12@users.noreply.github.com>
2025-02-02 23:15:12 -0600

    Finish lab 9

--- packages/labs/public/my-nav.mjs
+++ packages/labs/public/my-nav.mjs
@@ -1,5 +1,6 @@
 import { attachShadow } from "./utils.mjs";
 
+var expanded = false;
 const navItems = [
     { name: "Home", href: "index.html" },
     { name: "Fun Facts", href: "funfacts.html" },
@@ -8,39 +9,92 @@ const navItems = [
 
 let markup = `
   <style> 
-    a {
-    color: var(--color-link);
-}
-    nav {
+  button {
+    height: fit-content;
+    width: fit-content;
     display: flex;
-    min-width: 258px;
-    width: 50%;
+  }
+
+  #main-nav {
+    display: flex;
+    flex-direction: column;
+  }
+
+  #upper-items {
+    display: flex;
     flex-direction: row;
     align-items: center;
-    justify-content:space-around;
+    justify-content: space-between;
+  }
+
+  #lower-items {
+    display: none;
+    flex-direction: column;
+  }
+
+  @media (min-width: 650px) {
+    a {
+      color: var(--color-link);
+    }
+    #main-nav {
+      width: 100%;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-around;
     }
     .selected {
-    font-size: 1.6rem;
-}
-    button {
-    margin-right: 1em;
+      font-size: 1.6rem;
     }
-     </style>
-<nav id="main-nav">
-    <h1>Peter Ryan</h1>
-    ${navItems.map(item => 
+    button {
+      margin-right: 1em;
+    }
+    #lower-items {
+      display: flex !important;
+      flex-direction: row;
+      gap: 1em;
+    }
+    button {
+      display: none;
+    }
+  }
+  </style>
+
+  <nav id="main-nav">
+    <div id="upper-items">
+      <h1>Peter Ryan</h1>
+      <button id="toggle-btn">Menu</button>
+    </div>
+    <div id="lower-items">
+      ${navItems.map(item => 
         `<b><a href="${item.href}" ${window.location.href.includes(item.href) ? 'class="selected"' : ''}>${item.name}</a></b>`
-    ).join('')}
-    <button type = "button">Menu </button>
-</nav>`;
+      ).join('')}
+    </div>
+  </nav>`;
 
 const TEMPLATE = document.createElement("template");
 TEMPLATE.innerHTML = markup;
 
 class MyNav extends HTMLElement {
+    constructor() {
+        super();
+        // Attach shadow root ONCE
+        this.attachShadow({ mode: "open" });
+    }
+
     connectedCallback() {
-        const shadowRoot = attachShadow(this, TEMPLATE);
-        shadowRoot.append(TEMPLATE)
+        // Ensure we do not append the template multiple times
+        if (!this.shadowRoot.hasChildNodes()) {
+            this.shadowRoot.appendChild(TEMPLATE.content.cloneNode(true));
+
+            // Get elements inside shadow DOM
+            const toggleButton = this.shadowRoot.getElementById("toggle-btn");
+            const lowerDiv = this.shadowRoot.getElementById("lower-items");
+
+            toggleButton.addEventListener("click", () => {
+                expanded = !expanded;
+                lowerDiv.style.display = expanded ? "flex" : "none";
+            });
+        }
     }
 }
 
