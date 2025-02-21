b0ce23afb2b6e6badce8ad65bb90a20d589f2179
Peter Ryan <147430628+peterryan12@users.noreply.github.com>
2025-02-21 12:27:00 -0600

    Finish nested routing, img fethcing

--- packages/routing-lab/src/AccountSettings.jsx
+++ packages/routing-lab/src/AccountSettings.jsx
@@ -4,12 +4,12 @@ import { useState } from "react";
 export function AccountSettings(props) {
 
     return (
-        <MainLayout>
+        <div>
             <h2>Account settings</h2>
             <label>
                 Username <input onChange={(e) => {props.toggle(e.target.value)}}/>
             </label>
             <p><i>Changes are auto-saved.</i></p>
-        </MainLayout>
+        </div>
     );
 }
